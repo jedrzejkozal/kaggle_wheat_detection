f65e9f0425456baa39e2fc3a2641382aafa49084
jedrzejkozal <jedrzej.kozal@gmail.com>
2020-06-28 12:28:09 +0200

    refactor

--- torchvision_training.py
+++ torchvision_training.py
@@ -21,7 +21,25 @@ def main():
     writer = SummaryWriter()
     store_hyperparams(args, writer)
 
-    train(args, writer)
+    device = torch.device(args.device)
+    train_dataset, train_dataset_val, val_dataset, num_classes, mean, std = get_datasets(
+        args)
+    train_dataloader, train_dataloader_val, val_dataloader = get_dataloaders(
+        device, train_dataset, train_dataset_val, val_dataset, args)
+
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
+        pretrained=False, pretrained_backbone=False, num_classes=num_classes)
+    initilize_weights(model)
+    model = model.to(device)
+
+    optimizer = get_optimizer(args, model.parameters())
+
+    train(model, (train_dataloader, train_dataloader_val, val_dataloader),
+          optimizer, args.epochs, num_classes, writer, args.test_images_dir)
+
+    torch.save(model, 'faster_rcnn.pt')
+    preds = get_test_set_predictions(model, args.test_images_dir, mean, std)
+    save_test_set_predictions(preds, 'submission.csv')
 
 
 def parse_args():
@@ -57,7 +75,7 @@ def store_hyperparams(args, summary_writer):
     summary_writer.flush()
 
 
-def train(args, summary_writer):
+def get_datasets(args):
     mean, std = get_norm_stats(args.use_imagenet_norm_stats)
     train_transforms = get_train_transforms(
         args.min_area, args.min_visibility, mean, std)
@@ -65,61 +83,15 @@ def train(args, summary_writer):
         args.min_area, args.min_visibility, mean, std)
     train_dataset = WheatDataset(
         args.images_dir, args.train_csv_path, transforms=train_transforms)
+    train_dataset_val = WheatDataset(
+        args.images_dir, args.train_csv_path, transforms=val_transforms)
     val_dataset = WheatDataset(
         args.images_dir, args.val_csv_path, transforms=val_transforms)
     num_classes = len(train_dataset.classes) + 1  # include background
     print('train samples = ', len(train_dataset))
     print('val samples = ', len(val_dataset))
     print('num classes = ', num_classes)
-
-    device = torch.device(args.device)
-    collate_fn = functools.partial(collate, device=device)
-    train_dataloader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batch_size, collate_fn=collate_fn, num_workers=args.num_workers)
-    train_dataloader_val = torch.utils.data.DataLoader(
-        WheatDataset(
-            args.images_dir, args.train_csv_path, transforms=val_transforms), batch_size=args.batch_size, collate_fn=collate_fn, num_workers=args.num_workers)
-    val_dataloader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=args.batch_size, collate_fn=collate_fn, num_workers=args.num_workers)
-
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
-        pretrained=False, pretrained_backbone=False, num_classes=num_classes)
-    initilize_weights(model)
-    model = model.to(device)
-
-    optimizer = get_optimizer(args, model.parameters())
-
-    for epoch in range(args.epochs):
-        start = time.time()
-        model.train()
-        for img, target in train_dataloader:
-            output = model(img, target)
-            loss = sum(l for l in output.values())
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        duration = time.time() - start
-        print("epoch {}/{} duration {}: "
-              "loss_classifier={:.4f}, "
-              "loss_box_reg={:.4f}, "
-              "loss_objectness={:.4f}, "
-              "loss_rpn_box_reg={:.4f}"
-              .format(epoch + 1,
-                      args.epochs,
-                      time.strftime('%M:%S', time.gmtime(duration)),
-                      output['loss_classifier'].item(),
-                      output['loss_box_reg'].item(),
-                      output['loss_objectness'].item(),
-                      output['loss_rpn_box_reg'].item()))
-
-        save_metrics(model, train_dataloader_val,
-                     val_dataloader, summary_writer, epoch, num_classes)
-
-    torch.save(model, 'faster_rcnn.pt')
-    preds = get_test_set_predictions(model, args.test_images_dir, mean, std)
-    save_test_set_predictions(preds, 'submission.csv')
+    return train_dataset, train_dataset_val, val_dataset, num_classes, mean, std
 
 
 def get_norm_stats(use_imagenet_norm_stats):
@@ -173,6 +145,17 @@ def xmin_ymin_width_height_to_xmin_ymin_xmax_ymax(box):
     return torch.cat([box[:, :2], box[:, :2] + box[:, 2:]], dim=1)
 
 
+def get_dataloaders(device, train_dataset, train_dataset_val, val_dataset, args):
+    collate_fn = functools.partial(collate, device=device)
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, collate_fn=collate_fn, num_workers=args.num_workers)
+    train_dataloader_val = torch.utils.data.DataLoader(
+        train_dataset_val, batch_size=args.batch_size, collate_fn=collate_fn, num_workers=args.num_workers)
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=args.batch_size, collate_fn=collate_fn, num_workers=args.num_workers)
+    return train_dataloader, train_dataloader_val, val_dataloader
+
+
 def initilize_weights(model):
     def init(m):
         if type(m) == nn.Conv2d:
@@ -186,6 +169,37 @@ def get_optimizer(args, model_params):
         return optim.Adam(model_params, args.lr)
     elif optimizer_str == 'SGD':
         return optim.SGD(model_params, args.lr, momentum=args.momentum)
+
+
+def train(model, dataloaders, optimizer, epochs, num_classes, summary_writer, test_images_dir):
+    train_dataloader, train_dataloader_val, val_dataloader = dataloaders
+    for epoch in range(epochs):
+        start = time.time()
+        model.train()
+        for img, target in train_dataloader:
+            output = model(img, target)
+            loss = sum(l for l in output.values())
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        duration = time.time() - start
+        print("epoch {}/{} duration {}: "
+              "loss_classifier={:.4f}, "
+              "loss_box_reg={:.4f}, "
+              "loss_objectness={:.4f}, "
+              "loss_rpn_box_reg={:.4f}"
+              .format(epoch + 1,
+                      epochs,
+                      time.strftime('%M:%S', time.gmtime(duration)),
+                      output['loss_classifier'].item(),
+                      output['loss_box_reg'].item(),
+                      output['loss_objectness'].item(),
+                      output['loss_rpn_box_reg'].item()))
+
+        save_metrics(model, train_dataloader_val,
+                     val_dataloader, summary_writer, epoch, num_classes)
 
 
 def save_metrics(model, train_dataloader, val_dataloader, summary_writer, epoch, num_classes):
